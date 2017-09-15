b71084d02bdef6cc74107240595c3bcbe65b0c30
AndrewBieleski <aBieleski@gmail.com>
2017-09-15 22:09:18 +1200

    Fixed Move and Orientation

---
+++ Assets/Scripts/CharacterControler.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class CharacterControler : MonoBehaviour {
+
+    public float moveSpeed;
+    private Rigidbody2D thisRigidBody;
+
+    private Vector2 moveInput;
+    private Vector2 moveVelocity;
+
+	// Use this for initialization
+	void Start () {
+        thisRigidBody = GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        moveVelocity = moveInput * moveSpeed;
+	}
+
+    void FixedUpdate()
+    {
+        thisRigidBody.velocity = moveVelocity;
+    }
+}
